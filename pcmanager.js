25a19cfe9a3b50c2eb221c04490583b8b1ce916c
LimeBlogs <307485299+Aruki-Myu@users.noreply.github.com>
2026-08-10 10:38:22 +0800

    Update PCManager

--- pcmanager.js
+++ pcmanager.js
@@ -1,6 +1,5 @@
-import { saveSettingsDebounced, main_api } from '/script.js';
+import { saveSettingsDebounced } from '/script.js';
 import { promptManager } from '/scripts/openai.js';
-import { getPresetManager } from '/scripts/preset-manager.js';
 import { POPUP_TYPE, callGenericPopup, POPUP_RESULT, Popup } from '/scripts/popup.js';
 import { getSortableDelay, escapeHtml } from '/scripts/utils.js';
 
@@ -777,20 +776,10 @@ class PCManagerCore {
         }
 
         saveSettingsDebounced();
-        promptManager.render();
-
-        try {
-            const pm = getPresetManager(main_api);
-            if (pm) {
-                const selectedPresetName = pm.getSelectedPresetName();
-                if (selectedPresetName) {
-                    await pm.savePreset(selectedPresetName);
-                }
-            }
-        } catch (e) {
-            console.error('PCManager failed to save preset', e);
+        if ($('#update_oai_preset').length) {
+            $('#update_oai_preset').trigger('click');
         }
-
+        promptManager.render();
         toastr.success('PCManager: Changes committed.');
 
         this.diffs = [];
